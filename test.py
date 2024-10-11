15a1ec915b7910327c62496245f33e0ec4483607
Lucas0587 <sishuihetong@shu.edu.cn>
2024-10-11 17:52:54 +0800

    Initial Submit
    修改了进入页面的密码验证

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-import pandas ad pd
+import base64
+
+# 打开图片文件
+with open("背景图.jpg", "rb") as image_file:
+    # 读取图片内容
+    image_data = image_file.read()
+
+# 转换为Base64编码
+base64_string = base64.b64encode(image_data).decode('utf-8')
+
+# 输出Base64编码的字符串
+print(base64_string)
